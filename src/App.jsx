f5b28a1bd84c5758b1bd78090b42f6e5e043f089
Jonah <jonah.wakahiumaina@gmail.com>
2023-11-22 01:19:23 +0300

    added components and pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Home />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
@@ -16,10 +18,9 @@ function App() {
           {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
           <p>images</p>
           <p>Another commit meesage</p>
-          
         </a>
       </div>
-      <h1>Vite and  React</h1>
+      <h1>Vite and React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -32,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,5 @@
+import "./home.css";
+function Home() {
+  return <div>Home</div>;
+}
+export default Home;
